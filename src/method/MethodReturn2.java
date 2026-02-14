3d8dcc1615a254b0faf3a3b22e5fedb0365afef3
ki-sd <redringo4@gmail.com>
2026-02-14 20:04:35 +0900

    Method 실습 (Day10)

--- src/method/MethodReturn2.java
+++ src/method/MethodReturn2.java
@@ -10,7 +10,7 @@ public class MethodReturn2 {
     public static void checkAge(int age) {
         if (age < 18) {
             System.out.println(age + "살, 미성년자는 출입이 불가합니다.");
-            return;
+            return; //return 값을 만나면 메서드 종료
         }
         System.out.println(age + "살, 입장하세요.");
     }
