e8c62bd4bfb7fd8743b239da0e1056d581e8cdfd
ki-sd <redringo4@gmail.com>
2026-02-23 14:20:59 +0900

    객체지향 및 This (day15)

---
+++ java-start2/src/testPjt_Object/MainClass.java
@@ -0,0 +1,11 @@
+package testPjt_Object;
+
+public class MainClass {
+    public static void main(String[] args) {
+
+        int[] iArr = {10, 20, 30};
+        ObjectClass obj1 = new ObjectClass(10, 20);
+
+        obj1.getInfo();
+    }
+}
---
+++ java-start2/src/testPjt_Object/ObjectClass.java
@@ -0,0 +1,25 @@
+package testPjt_Object;
+
+public class ObjectClass {
+
+    public int x;
+    public int y;
+
+    public ObjectClass(String s, int[] iArr) {
+        System.out.println("Object Constructor");
+
+        System.out.println("s -----> " + s);
+        System.out.println("iArr -----> " + iArr);
+    }
+
+    public ObjectClass(int x, int y) {
+
+        this.x = x;
+        this.y = y;
+    }
+
+    public void getInfo() {
+        System.out.println("x ---> " + x);
+        System.out.println("y ---> " + y);
+    }
+}
