109b22534dd1f29455f2f13e345d61b41e5c4f72
ki-sd <redringo4@gmail.com>
2026-02-23 15:16:38 +0900

    객체지향 및 Getter,Setter (day15)

---
+++ java-start2/src/testPjt_SetGet/MainClass.java
@@ -0,0 +1,12 @@
+package testPjt_SetGet;
+
+public class MainClass {
+    public static void main(String[] args) {
+
+        Student student1 = new Student("홍길동", 90);
+        student1.getInfo();
+
+        student1.setScore(100);
+        student1.getInfo();
+    }
+}
